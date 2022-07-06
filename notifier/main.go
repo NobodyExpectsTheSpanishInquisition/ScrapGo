139f06a6709eb832f329a505a859a53ce64e7340
NobodyExpectsTheSpanishInquisition <s.miklaszewski1999@gmail.com>
2022-07-06 20:20:36 +0200

    load env to notifier

--- notifier/main.go
+++ notifier/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "github.com/spf13/viper"
 
+func main() {
+	loadEnv()
+}
+
+func loadEnv() {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return
+	}
 }
